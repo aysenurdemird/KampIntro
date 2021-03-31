e320859e5953e1d24d30643165893c611d5aa343
aishanourahadid <aysenurdemir.de@gmail.com>
2021-03-31 08:49:59 +0300

    Add project files.

---
+++ Kampİntro/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Kampİntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kategoriEtiketi = "Kategoriler";
+            int öğrenciSayısı = 32000;
+            double faizOrani = 1.45;    
+            bool sistemeGirisYaptiMi = true;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Azalış Butonu");
+            }
+            else if (dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Artış Butonu");
+            }
+            else
+            {
+                Console.WriteLine("değişmedi butonu");
+            }
+            
+            if (sistemeGirisYaptiMi == true)
+            {
+                Console.WriteLine("Kullanıcı ayaraları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+            
+            Console.WriteLine(kategoriEtiketi);
+        }
+    }   
+}
