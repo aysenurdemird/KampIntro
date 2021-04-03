29085cac1a65e73556b5cf108bf9502d9c5bfcfe
aishanourahadid <aysenurdemir.de@gmail.com>
2021-04-03 10:03:48 +0300

    new

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Congratulations, " + customer.Name + " " + customer.Surname + ". Your registration is completed.");
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine("We are sorry for that, " + customer.Name + " " + customer.Surname + ". Your registration has been deleted. We hope to see you again.");
+        }
+        
+        public void List(Customer[] customers)
+        {
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine(customer.Id);
+                Console.WriteLine(customer.Name);
+                Console.WriteLine(customer.Surname);
+            }
+
+            Console.WriteLine("Customers listed.");
+
+        }
+        
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,35 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Customer customer1 = new Customer();
+            customer1.Id = 875698;
+            customer1.Name = "Sude";
+            customer1.Surname = "Kaya";
+
+            Customer customer2 = new Customer();
+            customer2.Id = 987512;
+            customer2.Name = "Hasan";
+            customer2.Surname = "Avan";
+
+            Customer customer3 = new Customer();
+            customer3.Id = 157948;
+            customer3.Name = "Cemile";
+            customer3.Surname = "Bilgin";
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.Add(customer2);
+            customerManager.Add(customer3);
+
+            customerManager.Delete(customer1);      
+            customerManager.Delete(customer2);
+            customerManager.Delete(customer3);
+
+            Customer[] customers = new Customer[] { customer1, customer2, customer3 };
+            customerManager.List(customers);
+
+
+            //Console.WriteLine("Hello World!");
         }
     }
 }
